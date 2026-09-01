807299fcb01a2b0fbed24ed0256c44fdf5aa69ed
Muaz Ramzi <muazramzi.123@gmail.com>
2026-09-02 02:57:05 +0800

    feat(backend): add wishlist app

---
+++ styva_backend/apps/wishlist/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from apps.products.models import Product
+from apps.products.serializers import ProductListSerializer
+
+from .models import Wishlist
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        source='product',
+        write_only=True,
+    )
+
+    class Meta:
+        model = Wishlist
+        fields = ['id', 'product', 'product_id', 'created_at']
+        read_only_fields = ['id', 'created_at']
